7155a32283c53c31f96017aa77de00dbb6c02a13
Stone C. Lasley <stonelasley@gmail.com>
2018-07-27 20:48:08 -0600

    refactor(link.sh): Update paths

--- setup/link.sh
+++ setup/link.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 ln -sf ~/.dotfiles/.vim ~/.vim
 ln -f ~/.dotfiles/.vimrc ~/.vimrc
-ln -f ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -f ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -f ~/.dotfiles/git/gitignore ~/.gitignore
 ln -f ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -f ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.zsh ~/.zsh
 
