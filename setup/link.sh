307bae6e54dd0fa759d0cd6519fc176e83b96df7
Stone C. Lasley <stonelasley@gmail.com>
2018-08-02 18:33:07 -0600

    fix(setup): remove incorrect flag

--- setup/link.sh
+++ setup/link.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-ln -sTf ~/.dotfiles/.vim ~/.vim
-ln -sTf ~/.dotfiles/.vim/vimrc ~/.vimrc
-ln -sTf ~/.dotfiles/git/gitconfig ~/.gitconfig
-ln -sTf ~/.dotfiles/git/gitignore ~/.gitignore
-ln -sTf ~/.dotfiles/git/czrc ~/.czrc
-ln -sTf ~/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -sTf ~/.dotfiles/.mutt ~/.mutt
-ln -sTf ~/.dotfiles/.zsh ~/.zsh
-ln -sTf ~/.dotfiles/.zsh/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.vim ~/.vim
+ln -sf ~/.dotfiles/.vim/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/git/gitignore ~/.gitignore
+ln -sf ~/.dotfiles/git/czrc ~/.czrc
+ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/.mutt ~/.mutt
+ln -sf ~/.dotfiles/.zsh ~/.zsh
+ln -sf ~/.dotfiles/.zsh/zshrc ~/.zshrc
 
