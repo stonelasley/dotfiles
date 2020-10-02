0e621637340e01c3ff464d01b87178f755c97934
Stone C. Lasley <stonelasley@gmail.com>
2020-10-02 12:28:23 -0600

    feat(install): install ansible if absent

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 set -e
+
+if ! [ -x "$(command -v ansible)" ]; then
+  sudo apt install ansible
+fi
+
 git submodule update --init --recursive
 ansible-playbook --ask-become-pass main.yml "$@" 
