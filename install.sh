0571a1ebb10314deb2f6da46967827a2d42bdce5
Stone C. Lasley <stonelasley@gmail.com>
2021-07-24 06:06:26 -0600

    refactor(roles): package specific role

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 set -e
+if [ "$(uname -s)" = "Darwin" ] 
+then 
+  echo "Updating Brew..."
+  brew update
+  echo "Upgrading Brew..."
+  brew upgrade
+else
+  echo "Updating apt..."
+  sudo apt update
+  echo "Upgrading apt..."
+  sudo apt upgrade 
+fi
 
 if ! [ -x "$(command -v ansible)" ]; then
   if [ "$(uname -s)" = "Darwin" ]
@@ -9,6 +21,7 @@ if ! [ -x "$(command -v ansible)" ]; then
     echo "Installing Ansible..."
     brew install ansible
   else
+    echo "Updating apt..."
     sudo apt update
     sudo apt install ansible -y
   fi
