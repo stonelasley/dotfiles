4c41b947ef4e4f2d386fcd3003b74df402676caf
Stone C. Lasley <stonelasley@gmail.com>
2021-07-28 20:08:33 -0600

    fix(coc): mappings
    util method was setting noremap = true by default which doesn't
    work for mappings with <Plug>

--- nvim/lua/mappings/coc.lua
+++ nvim/lua/mappings/coc.lua
@@ -4,47 +4,48 @@ map('i', '<c-space>', 'coc#refresh()', { silent = true, expr = true })
 
 -- Use `[g` and `]g` to navigate diagnostics
 -- Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
-map('n', '[g', '<Plug>(coc-diagnostic-prev)', { silent = true })
-map('n', ']g', '<Plug>(coc-diagnostic-next)', { silent = true })
+map('n', '[g', '<Plug>(coc-diagnostic-prev)', { silent = true, noremap = false })
+map('n', ']g', '<Plug>(coc-diagnostic-next)', { silent = true, noremap = false })
 
 -- GoTo code navigation.
-map('n', 'gd', '<Plug>(coc-definition)', { silent = true })
-map('n', 'gy', '<Plug>(coc-type-definition)', { silent = true })
-map('n', 'gi', '<Plug>(coc-implementation)', { silent = true })
-map('n', 'gr', '<Plug>(coc-references)', { silent = true })
+map('n', 'gd', '<Plug>(coc-definition)', { silent = true, noremap = false })
+map('n', 'gy', '<Plug>(coc-type-definition)', { silent = true, noremap = false })
+map('n', 'gi', '<Plug>(coc-implementation)', { silent = true, noremap = false })
+map('n', 'gr', '<Plug>(coc-references)', { silent = true, noremap = false })
+
 
 -- TODO - ERROR <SID> not in a script context
 -- Use K to show documentation in preview window.
 -- map('n', '<silent> K', ':call <SID>show_documentation()<CR>')
 
 -- Symbol renaming.
-map('n', '<leader> rn', '<Plug>(coc-rename)')
+map('n', '<C-r><C-r>', '<Plug>(coc-rename)', { noremap = false })
 
 -- Formatting selected code.
-map('x', '<leader>f', '<Plug>(coc-format-selected)')
-map('n', '<leader>f', '<Plug>(coc-format-selected)')
-map('v', '<leader>f', '<Plug>(coc-format-selected)')
+map('x', '<leader>f', '<Plug>(coc-format-selected)', { noremap = false })
+map('n', '<leader>f', '<Plug>(coc-format-selected)', { noremap = false })
+map('v', '<leader>f', '<Plug>(coc-format-selected)', { noremap = false })
 
 -- Applying codeAction to the selected region.
 -- Example: `<leader>aap` for current paragraph
-map('x', '<leader>a', '<Plug>(coc-codeaction-selected)')
-map('n', '<leader>a', '<Plug>(coc-codeaction-selected)')
+map('x', '<leader>a', '<Plug>(coc-codeaction-selected)', { noremap = false })
+map('n', '<leader>a', '<Plug>(coc-codeaction-selected)', { noremap = false })
 
 -- Remap keys for applying codeAction to the current buffer.
-map('n', '<leader>ac', '<Plug>(coc-codeaction)')
+map('n', '<leader>ac', '<Plug>(coc-codeaction)', { noremap = false })
 -- Apply AutoFix to problem on the current line.
-map('n', '<leader>qf', '<Plug>(coc-fix-current)')
+map('n', '<leader>qf', '<Plug>(coc-fix-current)', { noremap = false })
 
 -- Map function and class text objects
 -- NOTE: Requires 'textDocument.documentSymbol' support from the language server.
-map('x', 'if', '<Plug>(coc-funcobj-i)')
-map('o', 'if', '<Plug>(coc-funcobj-i)')
-map('x', 'af', '<Plug>(coc-funcobj-a)')
-map('o', 'af', '<Plug>(coc-funcobj-a)')
-map('x', 'ic', '<Plug>(coc-classobj-i)')
-map('o', 'ic', '<Plug>(coc-classobj-i)')
-map('x', 'ac', '<Plug>(coc-classobj-a)')
-map('o', 'ac', '<Plug>(coc-classobj-a)')
+map('x', 'if', '<Plug>(coc-funcobj-i)', { noremap = false })
+map('o', 'if', '<Plug>(coc-funcobj-i)', { noremap = false })
+map('x', 'af', '<Plug>(coc-funcobj-a)', { noremap = false })
+map('o', 'af', '<Plug>(coc-funcobj-a)', { noremap = false })
+map('x', 'ic', '<Plug>(coc-classobj-i)', { noremap = false })
+map('o', 'ic', '<Plug>(coc-classobj-i)', { noremap = false })
+map('x', 'ac', '<Plug>(coc-classobj-a)', { noremap = false })
+map('o', 'ac', '<Plug>(coc-classobj-a)', { noremap = false })
 
 -- Remap <C-f> and <C-b> for scroll float windows/popups.
 map('n', '<C-f>', 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', { noremap = true, silent = true, nowait = true, expr = true})
