3670ea777685a0d72a0a0b12764effe7219bd90a
Stone C. Lasley <stonelasley@gmail.com>
2021-08-03 20:26:54 -0600

    style(lualine): tinkering with lualine

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -1,5 +1,39 @@
-require('lualine').setup {
+require'lualine'.setup {
   options = {
-    theme = 'neon'
-  }
+    icons_enabled = true,
+    theme = 'neon',
+    component_separators = {'', ''},
+    section_separators = {'', ''},
+    disabled_filetypes = {}
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = {'branch'},
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        path = 1
+      }
+    },
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {{'mode', color = nil}},
+    lualine_b = {},
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        path = 1
+      }
+    },
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
 }
