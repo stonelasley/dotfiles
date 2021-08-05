cc6c4cb40a4fba05271fcfe25577fa8597791c9d
Stone C. Lasley <stonelasley@gmail.com>
2021-08-04 19:43:57 -0600

    feat(lua-lsp): lsp config for lua

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,7 +1,7 @@
 local nvim_lsp = require('lspconfig')
 local buf_maps = require('mappings.lsp').set_buf_keymaps
 
-local servers = { "vuels", "omnisharp", "tsserver" }
+local servers = { "vuels", "omnisharp", "tsserver", "sumneko_lua" }
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
@@ -12,7 +12,7 @@ for _, ls in ipairs(servers) do
 
   nvim_lsp[ls].setup {
     on_attach = function(client, bufnr)
-      buf_maps(ls, bufnr) 
+      buf_maps(ls, bufnr)
       config.on_attach(client)
     end,
     flags = {
