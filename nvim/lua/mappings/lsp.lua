dcc8c12c42ad3d06bdd9b946225398571a4bf656
Stone C. Lasley <stonelasley@gmail.com>
2021-08-06 20:00:52 -0600

    refactor: mappings

--- nvim/lua/mappings/lsp.lua
+++ nvim/lua/mappings/lsp.lua
@@ -1,43 +1,42 @@
+local utils = require('utils')
 local M = {}
 
 function M.set_buf_keymaps(ls, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  utils.buf_opt(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
   local opts = { noremap=true, silent=true }
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  --buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gd', "<cmd>lua require('telescope.builtin').lsp_definitions(require('telescope.themes').get_cursor({}))<CR>", opts)
-  --buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', 'gr', "<cmd>lua require('telescope.builtin').lsp_references(require('telescope.themes').get_cursor({}))<CR>", opts)
-  --buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'gi', "<cmd>lua require('telescope.builtin').lsp_implementation(require('telescope.themes').get_cursor({}))<CR>", opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<leader><leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader><leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader><leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<C-r><C-r>', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  --buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', '<leader><leader>.', "<cmd>lua require('telescope.builtin').lsp_code_actions(require('telescope.themes').get_cursor({}))<CR>", opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<leader><leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '<leader><leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  utils.buf_map(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  utils.buf_map(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  --utils.buf_map(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  utils.buf_map(bufnr, 'n', 'gd', "<cmd>lua require('telescope.builtin').lsp_definitions(require('telescope.themes').get_cursor({}))<CR>", opts)
+  --utils.buf_map(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  utils.buf_map(bufnr, 'n', 'gr', "<cmd>lua require('telescope.builtin').lsp_references(require('telescope.themes').get_cursor({}))<CR>", opts)
+  --utils.buf_map(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  utils.buf_map(bufnr, 'n', 'gi', "<cmd>lua require('telescope.builtin').lsp_implementation(require('telescope.themes').get_cursor({}))<CR>", opts)
+  utils.buf_map(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  utils.buf_map(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  utils.buf_map(bufnr, 'n', '<leader><leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  utils.buf_map(bufnr, 'n', '<leader><leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  utils.buf_map(bufnr, 'n', '<leader><leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  utils.buf_map(bufnr, 'n', '<C-r><C-r>', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  --utils.buf_map(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  utils.buf_map(bufnr, 'n', '<leader><leader>.', "<cmd>lua require('telescope.builtin').lsp_code_actions(require('telescope.themes').get_cursor({}))<CR>", opts)
+  utils.buf_map(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  utils.buf_map(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  utils.buf_map(bufnr, 'n', '<leader><leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  utils.buf_map(bufnr, 'n', '<leader><leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 
   -- TODO - prettier should be firing but I'm getting completely diffrent
   -- formatting results
   if ls == 'vuels' then
-    buf_set_keymap("n", "<leader><leader>p", "<plug>(Prettier)<CR>", { silent = true })
+    utils.buf_map(bufnr, "n", "<leader><leader>p", "<plug>(Prettier)<CR>", { silent = true })
   elseif ls == 'tsserver' then
-    buf_set_keymap("n", "<leader><leader>p", "<plug>(Prettier)<CR>", opts)
+    utils.buf_map(bufnr, "n", "<leader><leader>p", "<plug>(Prettier)<CR>", opts)
   else
-    buf_set_keymap("n", "<leader><leader>p", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    utils.buf_map(bufnr, "n", "<leader><leader>p", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
   end
 end
 
--- nvim/lua/mappings/trouble.lua
+++ nvim/lua/mappings/trouble.lua
@@ -1,9 +1,9 @@
 local map = require('utils').map
-local opts = { silent = true }
+local opt = { silent = true }
 
-vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>", opt)
-vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble lsp_workspace_diagnostics<cr>", opt)
-vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble lsp_document_diagnostics<cr>", opt)
-vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>", opt)
-vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", opt)
-vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>", opt)
+map("n", "<leader>xx", "<cmd>Trouble<cr>", opt)
+map("n", "<leader>xw", "<cmd>Trouble lsp_workspace_diagnostics<cr>", opt)
+map("n", "<leader>xd", "<cmd>Trouble lsp_document_diagnostics<cr>", opt)
+map("n", "<leader>xl", "<cmd>Trouble loclist<cr>", opt)
+map("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", opt)
+map("n", "gR", "<cmd>Trouble lsp_references<cr>", opt)
