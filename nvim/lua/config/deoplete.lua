bec2d14b7fd2e4c1c5e64874587557cd858037bb
Stone C. Lasley <stonelasley@gmail.com>
2021-08-26 09:10:06 -0600

    fix(Deoplete): prevent completion in Telescope

--- nvim/lua/config/deoplete.lua
+++ nvim/lua/config/deoplete.lua
@@ -1,4 +1,3 @@
--- TODO - must be a better way
-require'compe'.setup { enabled = false}
 vim.api.nvim_exec([[call deoplete#enable()]], true)
 vim.api.nvim_exec([[call deoplete#custom#option('sources', { '_': ['buffer', 'ultisnips', 'file', 'dictionary'], 'cs': ['buffer', 'ultisnips', 'file', 'tag', 'omnisharp'] }) ]], true)
+vim.api.nvim_exec([[autocmd FileType TelescopePrompt call deoplete#custom#buffer_option('auto_complete', v:false) ]], true)
