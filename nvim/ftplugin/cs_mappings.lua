823c7ee9bf25e5f3bf0f64fa85cd06361bb98b64
Stone C. Lasley <stonelasley@gmail.com>
2021-08-29 13:24:42 -0600

    refactor(cs_mappings): port to Kartograaf

--- nvim/ftplugin/cs_mappings.lua
+++ nvim/ftplugin/cs_mappings.lua
@@ -1,28 +1,47 @@
-local map = require('utils').buf_map
 -- Omnisharp
+-- TODO
 -- cmd "autocmd FileType cs nmap <silent> <buffer> [[ <Plug>(omnisharp_navigate_up)"
 -- cmd "autocmd FileType cs nmap <silent> <buffer> ]] <Plug>(omnisharp_navigate_down)"
-
-local silent = { noremap = false, silent = true}
-map(0, 'n', 'gd', '<Plug>(omnisharp_go_to_definition)', silent)
-map(0, 'n', 'gi', '<Plug>(omnisharp_find_implementations)', silent)
-map(0, 'n', '<Leader>osfu', '<Plug>(omnisharp_find_usages)', silent)
-map(0, 'n', '<Leader>ospd', '<Plug>(omnisharp_preview_definition)', silent)
-map(0, 'n', '<Leader>ospi', '<Plug>(omnisharp_preview_implementations)', silent)
-map(0, 'n', '<Leader>ost', '<Plug>(omnisharp_type_lookup)', silent)
-map(0, 'n', '<Leader>osd', '<Plug>(omnisharp_documentation)', silent)
-map(0, 'n', '<Leader>osfs', '<Plug>(omnisharp_find_symbol)', silent)
-map(0, 'n', '<Leader><Leader>fu', '<Plug>(omnisharp_fix_usings)', silent)
-map(0, 'n', [[<C-\>]], [[<Plug>(omnisharp_signature_help)]], silent)
-map(0, 'i', [[<C-\>]], [[<Plug>(omnisharp_signature_help)]], silent)
-map(0, 'n', '<F12>', [[<Plug>(omnisharp_code_format)]], silent)
-map(0, 'i', '<F12>', [[<Plug>(omnisharp_code_format)]], silent)
-map(0, 'x', '<F12>', [[<Plug>(omnisharp_code_format)]], silent)
-map(0, 'n', '<C-r><C-r>', [[<Plug>(omnisharp_rename)]], silent)
-map(0, 'n', '<Leader><Leader>.', [[<Plug>(omnisharp_code_actions)]], silent)
-map(0, 'i', '<Leader>osgcc', [[<Plug>(omnisharp_global_code_check)]], silent)
-map(0, 'n', '<Leader>os.', [[<Plug>(omnisharp_code_actions_repeat)]], silent)
-map(0, 'x', '<Leader>os.', [[<Plug>(omnisharp_code_actions_repeat)]], silent)
-map(0, 'n', '<Leader>osre', [[<Plug>(omnisharp_restart_server)]], silent)
-map(0, 'n', '<Leader>osst', [[<Plug>(omnisharp_start_server)]], silent)
-map(0, 'n', '<Leader>ossp', [[<Plug>(omnisharp_stop_server)]], silent)
+--
+require('kartograaf').map({
+  buffer = 0,
+  options = { silent = true, noremap = false },
+  n = {
+    {
+      prefix = 'g',
+      { 'd', '<Plug>(omnisharp_go_to_definition)'},
+      { 'i', '<Plug>(omnisharp_find_implementations)'},
+    },
+    {
+      prefix = '<Leader>',
+      { 'osfu', '<Plug>(omnisharp_find_usages)' },
+      { 'ospd', '<Plug>(omnisharp_preview_definition)' },
+      { 'ospi', '<Plug>(omnisharp_preview_implementations)' },
+      { 'ost',  '<Plug>(omnisharp_type_lookup)' },
+      { 'osd',  '<Plug>(omnisharp_documentation)' },
+      { 'osfs', '<Plug>(omnisharp_find_symbol)' },
+      { 'os.',  '<Plug>(omnisharp_code_actions_repeat)' },
+      { 'osre', '<Plug>(omnisharp_restart_server)' },
+      { 'osst', '<Plug>(omnisharp_start_server)' },
+      { 'ossp', '<Plug>(omnisharp_stop_server)' },
+      { '<leader>fu', '<Plug>(omnisharp_fix_usings)' },
+      { '<leader>.', '<Plug>(omnisharp_code_actions)' },
+    },
+    { [[<C-\>]], [[<Plug>(omnisharp_signature_help)]] },
+    { '<F12>', [[<Plug>(omnisharp_code_format)]] },
+    { '<C-r><C-r>', [[<Plug>(omnisharp_rename)]] }
+  },
+  i = {
+    {
+      prefix = '<Leader>', { 'osgcc' , [[<Plug>(omnisharp_global_code_check)]] }
+    },
+    { [[<C-\>]], [[<Plug>(omnisharp_signature_help)]] },
+    { '<F12>', [[<Plug>(omnisharp_code_format)]] }
+  },
+  x = {
+    prefix = '<Leader>',
+    { 'osgcc' , [[<Plug>(omnisharp_global_code_check)]] },
+    { 'os.', '<Plug>(omnisharp_code_actions_repeat)' },
+    { '<F12>', [[<Plug>(omnisharp_code_format)]] }
+  }
+})
