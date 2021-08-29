106dc1db4d83f89dd7816019e26f51628d75f6f9
Stone C. Lasley <stonelasley@gmail.com>
2021-08-29 14:07:30 -0600

    refactor(trouble): use kartograaf

--- nvim/lua/mappings/trouble.lua
+++ nvim/lua/mappings/trouble.lua
@@ -1,8 +1,13 @@
-local map = require('utils').map
-local opt = { silent = true }
-
-map("n", "<leader>t", "<cmd>Trouble<cr>", opt)
-map("n", "<leader>tw", "<cmd>Trouble lsp_workspace_diagnostics<cr>", opt)
-map("n", "<leader>td", "<cmd>Trouble lsp_document_diagnostics<cr>", opt)
-map("n", "<leader>tl", "<cmd>Trouble loclist<cr>", opt)
-map("n", "<leader>tq", "<cmd>Trouble quickfix<cr>", opt)
+require('kartograaf').map({
+  n = {
+    options = { silent = true},
+    {
+      leader = '<leader>',
+      { 't', '<cmd>Trouble<CR>'},
+      { 'tw', '<cmd>Trouble lsp_workspace_diagnostics<CR>'},
+      { 'td', '<cmd>Trouble lsp_document_diagnostics<CR>'},
+      { 'tl', '<cmd>Trouble loclist<CR>'},
+      { 'tq', '<cmd>Trouble quickfix<CR>'},
+    }
+  }
+})
