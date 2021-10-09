ebcdcf8adcd3fc001324d951f01cd00ee7b0c563
Stone C. Lasley <stonelasley@gmail.com>
2021-10-09 09:55:27 -0600

    feat(vim): rust lsp

---
+++ nvim/lua/lsp/rust_analyzer.lua
@@ -0,0 +1,10 @@
+local M = {}
+function M.on_attach (client) 
+  --client.resolved_capabilities.document_formatting = true
+end
+
+M.cmd = {'rust-analyzer'}
+M.settings = {}
+return M
+--require('lspconfig').rust_analyzer.setup({})
+
