32ef700889b6aec81678a517f64d9a222de7df00
Stone C. Lasley <stonelasley@gmail.com>
2021-11-06 07:11:53 -0600

    plugin(github/copilot): I'm pretty excited

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -44,6 +44,7 @@ return require('packer').startup(function(use)
    use { 'hrsh7th/cmp-buffer' }
    use { 'hrsh7th/cmp-nvim-lsp' }
    use { 'hrsh7th/cmp-path' }
+   use { 'github/copilot.vim' }
    use { 'tzachar/cmp-tabnine',
         run='./install.sh',
         requires = 'hrsh7th/nvim-cmp',
