d49b4eaa4b147593bcaabc7ea284b40ab9e5a63d
Stone C. Lasley <stonelasley@gmail.com>
2022-01-20 20:14:00 -0700

    feat(cs): add color column at 100

--- nvim/ftplugin/cs.lua
+++ nvim/ftplugin/cs.lua
@@ -1,6 +1,6 @@
 require('cmp').setup.buffer { enabled = false }
 vim.api.nvim_exec([[ call ncm2#enable_for_buffer() ]], false)
 vim.cmd([[
-  set colorColumn=100
+  let colorColumn=100
   let b:ale_linters = { 'cs': ['OmniSharp'] } 
 ]])
