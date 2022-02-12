748e51ce8fd8ae34d31def44f978d6e630f4773d
Stone C. Lasley <stonelasley@gmail.com>
2022-02-11 18:42:03 -0700

    chore: stylua formatted

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,38 +1,38 @@
-pcall(require, 'impatient')
+pcall(require, "impatient")
 
 local cmd = vim.cmd
-local utils = require('utils')
+local utils = require("utils")
 
-cmd [[let &packpath = &runtimepath]]
-cmd [[set rtp+=/opt/fzf]]
+cmd([[let &packpath = &runtimepath]])
+cmd([[set rtp+=/opt/fzf]])
 
-utils.opt({'o', 'b'}, 'expandtab', true)
-utils.opt({'o', 'b'}, 'smartindent', true)
-utils.opt({'o', 'b'}, 'textwidth', 100)
-utils.opt({'o', 'w'}, 'number', true)
-utils.opt({'o', 'w'}, 'relativenumber', true)
-utils.opt('o', 'splitbelow', true)
-utils.opt('o', 'splitright', true)
-utils.opt('w', 'wrap', false)
-utils.opt('o', 'formatoptions', 'cro')
-utils.opt('o', 'termguicolors', true)
+utils.opt({ "o", "b" }, "expandtab", true)
+utils.opt({ "o", "b" }, "smartindent", true)
+utils.opt({ "o", "b" }, "textwidth", 100)
+utils.opt({ "o", "w" }, "number", true)
+utils.opt({ "o", "w" }, "relativenumber", true)
+utils.opt("o", "splitbelow", true)
+utils.opt("o", "splitright", true)
+utils.opt("w", "wrap", false)
+utils.opt("o", "formatoptions", "cro")
+utils.opt("o", "termguicolors", true)
 vim.opt.clipboard:append("unnamedplus")
-vim.g.indentLine_fileTypeExclude = {'json'}
+vim.g.indentLine_fileTypeExclude = { "json" }
 vim.g.wrap = false
-vim.g.ttimeoutlen=100
-vim.g.guicursor='blinkon100'
-vim.o.completeopt="menuone,preview,noinsert"
+vim.g.ttimeoutlen = 100
+vim.g.guicursor = "blinkon100"
+vim.o.completeopt = "menuone,preview,noinsert"
 
 -- Highlight text on yank
 vim.api.nvim_exec(
-  [[
+	[[
   augroup YankHighlight
     autocmd!
     autocmd TextYankPost * silent! lua vim.highlight.on_yank()
   augroup end
   ]],
-  false
+	false
 )
 
-require('plugins')
+require("plugins")
 vim.g.mapleader = " "
