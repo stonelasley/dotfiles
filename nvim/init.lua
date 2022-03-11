c8ff9407453fd537be6014f1f1a852a419b644d4
Stone C. Lasley <stonelasley@gmail.com>
2022-03-10 19:11:43 -0700

    feat(impatient): enable profile

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,8 @@
-pcall(require, 'impatient')
+local present, impatient = pcall(require, 'impatient')
+
+if present then
+  impatient.enable_profile()
+end
 
 require('st1.options')
 require('plugins')
