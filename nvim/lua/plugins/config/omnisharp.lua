e95c4bfa40afedcfdd0a379be1c376848ddc4b97
Stone C. Lasley <stonelasley@gmail.com>
2022-04-03 06:31:33 -0600

    refactor(omnisharp): use Treesitter highlights

--- nvim/lua/plugins/config/omnisharp.lua
+++ nvim/lua/plugins/config/omnisharp.lua
@@ -10,7 +10,31 @@ vim.cmd([[
   let g:OmniSharp_autoselect_existing_sln = 1
   let g:OmniSharp_popup_position = 'atcursor'
   let g:OmniSharp_want_snippet = 1
-  let g:mniSharp_highlighting = 1
+  let g:OmniSharp_highlighting = 1
   let g:OmniSharp_diagnostic_exclude_paths = [ 'Temp[/\\]', 'obj[/\\]', '\.nuget[/\\]', 'Migrations\]' ]
   let g:OmniSharp_popup_mappings = { 'sigNext': '<C-n>', 'sigPrev': '<C-p>', 'lineDown': ['<C-e>', 'j'], 'lineUp': ['<C-y>', 'k'] }
+  
+  let g:OmniSharp_highlight_groups = {
+  \ 'Comment': 'TSComment',
+  \ 'FieldName' : 'TSField',
+  \ 'MethodName' : 'TSMethod',
+  \ 'ExtentionMethodName' : 'TSFunction',
+  \ 'Operator' : 'TSOperator',
+  \ 'Keyword' : 'TSKeyword',
+  \ 'StringLiteral' : 'TSString',
+  \ 'LocalName' : 'TSTitle',
+  \ 'PropertyName' : 'TSProperty',
+  \ 'VerbatimeStringLiteral' : 'TSLiteral',
+  \ 'EnumName' : 'TSType',
+  \ 'Punctuation' : 'TSPunctSpecial',
+  \ 'ClassName' : 'TSTypeBuiltin',
+  \ 'RecordName' : 'TSType',
+  \ 'StructName' : 'TSType',
+  \ 'ConstantName' : 'TSConstant',
+  \ 'NamespaceName' : 'TSInclude',
+  \ 'ParameterName' : 'TSParameter',
+  \ 'XmlDocCommentName': 'Identifier',
+  \ 'XmlDocCommentText': 'NonText',
+  \ 'LabelName': 'TSLabel',
+  \}
 ]])
