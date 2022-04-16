a92dfcec577e89320c2bfa710618924dc6b6bf66
Stone C. Lasley <stonelasley@gmail.com>
2022-04-16 09:03:26 -0600

    fix(beacon): invalid pattern errror on buffer ignore

--- nvim/lua/plugins/config/beacon.lua
+++ nvim/lua/plugins/config/beacon.lua
@@ -5,7 +5,7 @@ vim.api.nvim_exec(
     let g:beacon_minimal_jump = 10
     let g:beacon_shrink = 1
     let g:beacon_fade = 1
-    let g:beacon_ignore_buffers = [\w*git*\w]
+    "let g:beacon_ignore_buffers = [\w*git*\w]
     let g:beacon_ignore_filetypes = ['fzf']
   ]],
   false
