1b628f5d9bd90492c3797b6c14886a242d2e68e8
Stone C. Lasley <stonelasley@gmail.com>
2022-08-03 06:15:35 -0600

    refactor(lsp): remove rust ftplugin

--- nvim/ftplugin/lua.lua
+++ nvim/ftplugin/lua.lua
@@ -1,12 +1,12 @@
-require('kartograaf').map({
-  n = {
-    buffer = 0,
-    options = { noremap = false },
-    -- { '<F12>', '<cmd>lua vim.lsp.buf.formatting()<CR>' },
-    {
-      prefix = '<leader><leader>',
-      { 'rt', '<Plug>PlenaryTestFile' },
-      { 'so', ':luafile %<CR>', { silent = true } },
-    },
-  },
-})
+--require('kartograaf').map({
+--  n = {
+--    buffer = 0,
+--    options = { noremap = false },
+--    -- { '<F12>', '<cmd>lua vim.lsp.buf.formatting()<CR>' },
+--    {
+--      prefix = '<leader><leader>',
+--      { 'rt', '<Plug>PlenaryTestFile' },
+--      { 'so', ':luafile %<CR>', { silent = true } },
+--    },
+--  },
+--})
--- nvim/lua/plugins/config/lsp/null-ls/init.lua
+++ nvim/lua/plugins/config/lsp/null-ls/init.lua
@@ -13,20 +13,20 @@ end
 local sources = {
   -- formatting
   b.formatting.prettierd,
-  b.formatting.shfmt,
-  b.formatting.shellharden,
-  b.formatting.fixjson,
-  b.formatting.black.with { extra_args = { "--fast" } },
-  b.formatting.isort,
+  --b.formatting.shfmt,
+  --b.formatting.shellharden,
+  --b.formatting.fixjson,
+  --b.formatting.black.with { extra_args = { "--fast" } },
+  --b.formatting.isort,
   b.formatting.stylua,
-  b.formatting.google_java_format,
+  --b.formatting.google_java_format,
   -- with_root_file(b.formatting.stylua, "stylua.toml"),
 
   -- diagnostics
-  b.diagnostics.write_good,
+  --b.diagnostics.write_good,
   -- b.diagnostics.markdownlint,
   b.diagnostics.eslint_d,
-  b.diagnostics.flake8.with { extra_args = { "--max-line-length=100"}},
+  --b.diagnostics.flake8.with { extra_args = { "--max-line-length=100"}},
   b.diagnostics.tsc,
   -- b.diagnostics.selene,
   -- b.diagnostics.codespell,
@@ -39,13 +39,13 @@ local sources = {
   -- b.diagnostics.stylelint,
 
   -- code actions
-  b.code_actions.gitsigns.with {
-    disabled_filetypes = { "NeogitCommitMessage" },
-  },
+  --b.code_actions.gitsigns.with {
+  --  disabled_filetypes = { "NeogitCommitMessage" },
+  --},
   b.code_actions.eslint_d,
   b.code_actions.gitrebase,
   b.code_actions.refactoring,
-  b.code_actions.proselint,
+  --b.code_actions.proselint,
   b.code_actions.shellcheck,
 
   -- hover
