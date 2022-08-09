f5679745ab65d2cc51f6f21849d3181c2a570b38
Stone C. Lasley <stonelasley@gmail.com>
2022-08-08 19:39:33 -0600

    fix(LspKind): fix config

--- nvim/lua/plugins/config/cmp.lua
+++ nvim/lua/plugins/config/cmp.lua
@@ -1,5 +1,6 @@
 local cmp = require "cmp"
 local ls = require "luasnip"
+local lspkind = require "lspkind"
 
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -52,5 +53,18 @@ cmp.setup {
         fallback()
       end
     end, { "i", "s" }),
+  },
+  formatting = {
+    format = lspkind.cmp_format {
+      mode = "symbol", -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      -- before = function (entry, vim_item)
+      -- ...
+      -- return vim_item
+      -- end
+    },
   },
 }
