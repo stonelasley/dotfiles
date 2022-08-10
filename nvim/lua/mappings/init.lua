ad3fb3b8d633d12a34e276e4925546009b528158
Stone C. Lasley <stonelasley@gmail.com>
2022-08-09 18:57:34 -0600

    refactor(mappings): seperate mapping config

---
+++ nvim/lua/mappings/init.lua
@@ -0,0 +1,79 @@
+local M = {}
+
+M.normal= {
+  f = {
+    [[<cmd>lua require('hop').hint_char1({ direction = require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = false })<cr>]],
+    "Hop forward",
+  },
+  F = {
+    [[<cmd>lua require('hop').hint_char1({ direction = require('hop.hint').HintDirection.BEFORE_CURSOR, current_line_only = false })<cr>]],
+    "Hop backward",
+  },
+  [","] = { ",<c-g>u", "break undo ','" },
+  ["."] = { ".<c-g>u", "break undo '.'" },
+  [";"] = { ";<c-g>u", "break undo ';'" },
+  ["<C-h>"] = { "<cmd> :TmuxNavigateLeft<CR>", "TmuxNavLeft" },
+  ["<C-j>"] = { "<cmd> :TmuxNavigateDown<CR>", "TmuxNavigateDown " },
+  ["<C-k>"] = { "<cmd> :TmuxNavigateUp<CR>", "TmuxNavigateUp" },
+  ["<C-l>"] = { "<cmd> :TmuxNavigateRight<CR>", "TmuxNavigateRight" },
+  ["<C-n>"] = { "<cmd> :NvimTreeToggle<CR>", "פּ Nvim Tree" },
+  ["<C-p>"] = { "<cmd>Telescope find_files<CR>", " Find Files" },
+  ["<C-f>"] = { "<cmd> :Telescope live_grep<CR>", " Live Grep" },
+  ["<C-=>"] = { "zm", " Zoom In" },
+  ["<C-->"] = { "zr", " Zoom Out" },
+  ["<M-h>"] = { "<cmd>:vertical resize+2<CR>", "Vertical Resize +2" },
+  ["<M-j>"] = { "<cmd>:resize-2<CR>", "Resize -2" },
+  ["<M-k>"] = { "<cmd>:resize+2<CR>", "Resize +2" },
+  ["<M-l>"] = { "<cmd>:vertical resize-2<CR>", "Vertical Resize -2" },
+}
+
+M.normal_leader = {
+  g = {
+    name = " +Git",
+    s = { "<cmd>Git<cr>", " Status" },
+    c = { "<cmd>Telescope git_bcommits<CR>", " Commits" },
+    b = { "<cmd>Telescope git_branches<CR>", " Branches" },
+    p = { "<cmd>Git push<cr>", " push" },
+    u = { "<cmd>Git pull<cr>", " pull" },
+  },
+  y = { '"+y', "yank" },
+  d = { '"_d', "delete" },
+  Y = { 'gg"+yG', "Select All" },
+  t = {
+    name = " +Telescope",
+    f = { "<cmd>Telescope find_files<CR>", " Find Files" },
+    g = { "<cmd>Telescope live_grep<CR>", " Live Grep" },
+    b = { "<cmd>Telescope buffers<CR>", "﬘ Buffers" },
+    c = { "<cmd>Telescope command_history<CR>", " Commands" },
+    t = { "<cmd>Telescope tags<CR>", " Tags" },
+    o = { "<cmd>Telescope vim_options<CR>", " Vim Options" },
+    r = { "<cmd>Telescope reloader<CR>", "勒 Reload" },
+    h = { "<cmd>Telescope help_tags<CR>", "ﬤ Help Tags" },
+    s = { "<cmd>:lua require('st1.telescope').focus_find()<CR>", " Dot Files" },
+  },
+  P = {
+    name = " +Packer",
+    s = { "<cmd>PackerStatus<CR>", "  Status" },
+    u = { "<cmd>PackerUpdate<CR>", "מּ  Update" },
+    i = { "<cmd>PackerInstall<CR>", "  Install" },
+    c = { "<cmd>PackerCompile<CR>", "  Compile" },
+    l = { "<cmd>PackerClean<CR>", "  Clean" },
+    y = { "<cmd>PackerSync<CR>", "מּ  Sync" },
+  },
+}
+
+M.vis_sel_leader = {
+  p = { '"_dP', "delete" },
+  y = { '"+y', "yank" },
+}
+
+M.visual_leader = {
+  p = { '"_dP', "delete" },
+  y = { '"+y', "yank" },
+}
+
+M.command = {
+  ["w!!"] = { "w !sudo tee %", "Sudo Write" },
+}
+
+return M
--- nvim/lua/plugins/config/lsp/mappings.lua
+++ nvim/lua/plugins/config/lsp/mappings.lua
@@ -1,90 +1,17 @@
 local M = {}
 
 local whichkey = require "which-key"
-
-local map = vim.keymap.set
-
-local function keymappings(client, bufnr)
-  local opts = { noremap = true, silent = true }
-
-  -- Key mappings
-  map("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
-  map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-  map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-  map("n", "[e", "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.ERROR})<CR>", opts)
-  map("n", "]e", "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR})<CR>", opts)
-  map("n", "<F2>", "<cmd>Lspsaga rename<CR>", opts)
-
-  if client.server_capabilities.documentFormattingProvider then
-    map("n", "<F12>", "<cmd>lua vim.lsp.buf.format({async = true})<CR>", opts)
-  end
-
-  -- Whichkey
-  local keymap_l = {
-    l = {
-      name = "+LSP",
-      -- a = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Code Action" },
-      a = { "<cmd>Lspsaga code_action<CR>", "Code Action" },
-      d = { "<cmd>Telescope diagnostics<CR>", "Diagnostics" },
-      f = { "<cmd>Lspsaga lsp_finder<CR>", "Finder" },
-      i = { "<cmd>LspInfo<CR>", "Lsp Info" },
-      r = { "<cmd>Telescope lsp_references<CR>", "References" },
-      s = { "<cmd>Telescope lsp_document_symbols<CR>", "Document Symbols" },
-      t = { "<cmd>TroubleToggle document_diagnostics<CR>", "Trouble" },
-      L = { "<cmd>lua vim.lsp.codelens.refresh()<CR>", "Refresh CodeLens" },
-      l = { "<cmd>lua vim.lsp.codelens.run()<CR>", "Run CodeLens" },
-      D = { "<cmd>lua require('config.lsp').toggle_diagnostics()<CR>", "Toggle Inline Diagnostics" },
-      W = { "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", "Add Workspace Folder" },
-      X = { "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", "Remove Workspace Folder" },
-      F = {
-        "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-        "List Workspace Folder",
-      },
-      R = { "<cmd>Trouble lsp_references<CR>", "Trouble References" },
-    },
-  }
-
-  local keymap_g = {
-    name = "+Goto",
-    -- d = { "<Cmd>lua vim.lsp.buf.definition()<CR>", "Definition" },
-    d = { "<cmd><cmd>Lspsaga preview_definition<CR>", "Definition" },
-    D = { "<Cmd>lua vim.lsp.buf.declaration()<CR>", "Declaration" },
-    h = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help" },
-    I = { "<cmd>Telescope lsp_implementations<CR>", "Goto Implementation" },
-    i = {
-      "<cmd>lua require('telescope.builtin').lsp_implementations(require('telescope.themes').get_cursor({}))<CR>",
-      "Goto Implementation",
-    },
-    b = { "<cmd>lua vim.lsp.buf.type_definition()<CR>", "Goto Type Definition" },
-    r = {
-      "<cmd>lua require('telescope.builtin').lsp_references(require('telescope.themes').get_cursor({}))<CR>",
-      "References",
-    },
-    R = { "<cmd>Trouble lsp_references<CR>", "Trouble References" },
-
-    --b = { "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>", "Goto Type Definition" },
-  }
-
-  local keymap_v_l = {
-    l = {
-      name = "LSP",
-      a = { "<cmd>Lspsaga code_action<CR>", "Code Action" },
-    },
-  }
-
-  local o = { buffer = bufnr, prefix = "<leader>" }
-  whichkey.register(keymap_l, o)
-
-  o = { mode = "v", buffer = bufnr, prefix = "<leader>" }
-  whichkey.register(keymap_v_l, o)
-
-  o = { buffer = bufnr, prefix = "g" }
-  whichkey.register(keymap_g, o)
-end
+local maps = require('mappings/lsp')
 
 function M.setup(client, bufnr)
-  keymappings(client, bufnr)
+  maps.setup(client, bufnr)
   -- signature_help(client, bufnr) -- use cmp-nvim-lsp-signature-help
+  local o = { buffer = bufnr, prefix = "<leader>" }
+  whichkey.register(maps.n, o)
+  o = { mode = "v", buffer = bufnr, prefix = "<leader>" }
+  whichkey.register(maps.v, o)
+  o = { buffer = bufnr, prefix = "g" }
+  whichkey.register(maps.g, o)
 end
 
 return M
