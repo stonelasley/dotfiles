9f2514274ba3f262c5952a3932f6815b4e90a5a9
Stone C. Lasley <stonelasley@gmail.com>
2022-12-23 12:20:53 -0700

    chore(plugins.lua): rearrange, comment, +copilot

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,8 +1,179 @@
 local plugins = {
+  -- Util
   { "lewis6991/impatient.nvim" },
   {
     "wbthomason/packer.nvim",
     event = "VimEnter",
+  },
+  -- Search / Find
+  { "wincent/loupe" },
+  {
+    "kyazdani42/nvim-tree.lua",
+    after = "nvim-web-devicons",
+    requires = { { "kyazdani42/nvim-web-devicons" } },
+    config = [[require('plugins.config.nvim-tree')]],
+  },
+  { "nvim-telescope/telescope-fzy-native.nvim" },
+  {
+    "nvim-telescope/telescope-frecency.nvim",
+    requires = { { "tami5/sql.nvim" } },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
+    config = [[require('plugins.config.telescope')]],
+  },
+
+  -- Completion
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-copilot" },
+  { "saadparwaiz1/cmp_luasnip" },
+  {
+    "hrsh7th/nvim-cmp",
+    config = [[require('plugins.config.cmp')]],
+  },
+  { "github/copilot.vim" },
+  -- Movement
+  {
+    "phaazon/hop.nvim",
+    as = "hop",
+    config = [[require('plugins.config.hop')]],
+  },
+  { "christoomey/vim-tmux-navigator" },
+  { "dhruvasagar/vim-zoom" },
+  {
+    "prettier/vim-prettier",
+    run = "yarn install",
+    ft = {
+      "css",
+      "html",
+      "javascript",
+      "scss",
+      "typescript",
+      "vue",
+    },
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    config = [[require('plugins.config.luasnip')]],
+  },
+  {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = [[require('plugins.config.todo')]],
+  },
+
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    config = [[require('plugins.config.lsp').setup()]],
+    requires = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "jose-elias-alvarez/null-ls.nvim",
+      "folke/neodev.nvim",
+      "b0o/schemastore.nvim",
+      "hoffs/omnisharp-extended-lsp.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      "jayp0521/mason-null-ls.nvim",
+      {
+        "simrat39/inlay-hints.nvim",
+        config = [[require('plugins.config.inlay-hints')]],
+      },
+      {
+        "theHamsta/nvim-semantic-tokens",
+        config = [[require('plugins.config.semantic-tokens').setup()]],
+      },
+    },
+  },
+  {
+    "maan2003/lsp_lines.nvim",
+    config = [[require('plugins.config.lsp_lines')]],
+  },
+  {
+    "glepnir/lspsaga.nvim",
+    config = [[require('plugins.config.lspsaga')]],
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufRead", "BufNewFile" },
+    run = ":TSUpdate",
+    config = [[require('plugins.config.treesitter')]],
+  },
+  {
+    "nvim-treesitter/playground",
+    requires = { { "nvim-treesitter/nvim-treesitter" } },
+    config = [[require('plugins.config.treesitterplayground')]],
+    cmd = "TSPlaygroundToggle",
+  },
+  {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = [[require('plugins.config.trouble')]],
+  },
+  {
+    "onsails/lspkind-nvim",
+    config = [[require('plugins.config.lspkind')]],
+  },
+
+  -- Debugging
+  {
+    "mfussenegger/nvim-dap",
+    ft = { "cs", "rs" },
+  },
+
+
+  -- Vue
+  { "posva/vim-vue", ft = { "typescript", "vue" } },
+  -- Rust
+  {
+    "simrat39/rust-tools.nvim",
+    requires = { "nvim-lua/plenary.nvim", "rust-lang/rust.vim" },
+    module = "rust-tools",
+  },
+  -- Typescript
+  {
+    "leafgarland/typescript-vim",
+    config = [[require('plugins.config.typescript')]],
+  },
+
+  -- Theme
+  {
+    "rmehri01/onenord.nvim",
+    config = [[require('plugins.config.theme')]],
+  },
+  {
+    "hoob3rt/lualine.nvim",
+    requires = { { "kyazdani42/nvim-web-devicons" } },
+    config = [[require('plugins.config.lualine')]],
+  },
+
+  -- Misc
+  { "tpope/vim-fugitive" },
+  {
+    "stonelasley/flare.nvim",
+    config = [[require('plugins.config.flare')]],
+  },
+  {
+    "nvim-orgmode/orgmode",
+    config = [[require('plugins.config.orgmode')]],
+  },
+  { "RishabhRD/popfix" },
+  {
+    "RishabhRD/nvim-cheat.sh",
+    requires = { { "RishabhRD/popfix" } },
+    event = { "BufRead", "BufNewFile" },
+  },
+  {
+    "folke/which-key.nvim",
+    config = [[require('plugins.config.whichkey')]],
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    tag = "v2.*",
+    config = [[require('plugins.config.toggleterm')]],
   },
   { "nathom/filetype.nvim" },
   {
@@ -34,174 +205,6 @@ local plugins = {
     config = [[require('plugins.config.indentguides')]],
     event = "BufRead",
   },
-  {
-    "stonelasley/flare.nvim",
-    -- "~/Projects/st1/flare.nvim",
-    config = [[require('plugins.config.flare')]],
-  },
-  { "wincent/loupe" },
-  {
-    "kyazdani42/nvim-tree.lua",
-    after = "nvim-web-devicons",
-    requires = { { "kyazdani42/nvim-web-devicons" } },
-    config = [[require('plugins.config.nvim-tree')]],
-  },
-  { "nvim-telescope/telescope-fzy-native.nvim" },
-  {
-    "nvim-telescope/telescope-frecency.nvim",
-    requires = { { "tami5/sql.nvim" } },
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
-    config = [[require('plugins.config.telescope')]],
-  },
-  { "hrsh7th/cmp-buffer" },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "hrsh7th/cmp-path" },
-  { "saadparwaiz1/cmp_luasnip" },
-  {
-    "hrsh7th/nvim-cmp",
-    config = [[require('plugins.config.cmp')]],
-  },
-  {
-    "phaazon/hop.nvim",
-    as = "hop",
-    config = [[require('plugins.config.hop')]],
-  },
-  { "christoomey/vim-tmux-navigator" },
-  { "dhruvasagar/vim-zoom" },
-  { "tpope/vim-fugitive" },
-  {
-    "prettier/vim-prettier",
-    run = "yarn install",
-    ft = {
-      "css",
-      "html",
-      "javascript",
-      "scss",
-      "typescript",
-      "vue",
-    },
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    config = [[require('plugins.config.luasnip')]],
-  },
-  {
-    "mfussenegger/nvim-dap",
-    ft = { "cs", "rs" },
-  },
-  {
-    "maan2003/lsp_lines.nvim",
-    config = [[require('plugins.config.lsp_lines')]],
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = [[require('plugins.config.lsp').setup()]],
-    requires = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "jose-elias-alvarez/null-ls.nvim",
-      "folke/neodev.nvim",
-      "b0o/schemastore.nvim",
-      "hoffs/omnisharp-extended-lsp.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-      "jayp0521/mason-null-ls.nvim",
-      {
-        "simrat39/inlay-hints.nvim",
-        config = [[require('plugins.config.inlay-hints')]],
-      },
-      {
-        "theHamsta/nvim-semantic-tokens",
-        config = [[require('plugins.config.semantic-tokens').setup()]],
-      },
-    },
-  },
-  {
-    "folke/todo-comments.nvim",
-    requires = "nvim-lua/plenary.nvim",
-    config = [[require('plugins.config.todo')]],
-  },
-  -- { "~/Projects/st1/dotnet.nvim" },
-  {
-    "glepnir/lspsaga.nvim",
-    config = [[require('plugins.config.lspsaga')]],
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufRead", "BufNewFile" },
-    run = ":TSUpdate",
-    config = [[require('plugins.config.treesitter')]],
-  },
-  {
-    "nvim-treesitter/playground",
-    requires = { { "nvim-treesitter/nvim-treesitter" } },
-    config = [[require('plugins.config.treesitterplayground')]],
-    cmd = "TSPlaygroundToggle",
-  },
-  {
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
-    config = [[require('plugins.config.trouble')]],
-  },
-  -- {
-  --   "omnisharp/omnisharp-vim",
-  --   config = [[require('plugins.config.omnisharp')]],
-  --   ft = { "cs" },
-  --   disabled = true,
-  -- },
-
-  { "euclidianace/betterlua.vim", ft = { "lua" } },
-  {
-    "rafcamlet/nvim-luapad",
-    config = [[require('plugins.config.luapad')]],
-    ft = { "lua" },
-  },
-  --use "tjdevries/nlua.nvim"
-
-  { "posva/vim-vue", ft = { "typescript", "vue" } },
-  {
-    "simrat39/rust-tools.nvim",
-    requires = { "nvim-lua/plenary.nvim", "rust-lang/rust.vim" },
-    module = "rust-tools",
-  },
-  {
-    "leafgarland/typescript-vim",
-    config = [[require('plugins.config.typescript')]],
-  },
-  {
-    "hoob3rt/lualine.nvim",
-    requires = { { "kyazdani42/nvim-web-devicons" } },
-    config = [[require('plugins.config.lualine')]],
-  },
-  {
-    "onsails/lspkind-nvim",
-    config = [[require('plugins.config.lspkind')]],
-  },
-  {
-    "rmehri01/onenord.nvim",
-    config = [[require('plugins.config.theme')]],
-  },
-  {
-    "nvim-orgmode/orgmode",
-    config = [[require('plugins.config.orgmode')]],
-  },
-  { "RishabhRD/popfix" },
-  {
-    "RishabhRD/nvim-cheat.sh",
-    requires = { { "RishabhRD/popfix" } },
-    event = { "BufRead", "BufNewFile" },
-  },
-  {
-    "folke/which-key.nvim",
-    config = [[require('plugins.config.whichkey')]],
-  },
-  {
-    "akinsho/toggleterm.nvim",
-    tag = "v2.*",
-    config = [[require('plugins.config.toggleterm')]],
-  },
 }
 
 local packer = require "packer"
@@ -220,7 +223,7 @@ packer.init {
       return require("packer.util").float { border = "single" }
     end,
   },
-  log = { level = 'warn' },
+  log = { level = "warn" },
 }
 packer.reset()
 -- packer.use(plugins)
