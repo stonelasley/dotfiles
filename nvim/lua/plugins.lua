bc4de2aa2a4fdb1cf0caf187012640cd1909bb20
Stone C. Lasley <stone@lasley.io>
2023-05-09 16:41:40 -0600

    chore(plugins.lua): update chatGPT config
    chore: update all plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -302,7 +302,9 @@ local plugins = {
   },
   {
     "jackMort/ChatGPT.nvim",
-    config = true,
+    config = function()
+      require("chatgpt").setup()
+    end,
     dependencies = {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
