2b9ce9603f1020bfe7fa5a2d576a8553e14d807e
Stone C. Lasley <stone@lasley.io>
2024-06-05 13:06:52 -0600

    feat: add copilot

--- lazy/lua/plugins/copilot.lua
+++ lazy/lua/plugins/copilot.lua
@@ -1,18 +1,14 @@
 return {
   {
-    "github/copilot.vim",
-    config = function(_, opts)
-      vim.cmd([[
-        let g:copilot_filetypes = {
-          \ 'telescope': v:false,
-          \ 'TelescopePrompt': v:false,
-          \ 'TelescopeResult': v:false,
-          \ 'help': v:false,
-          \ }
-        imap <silent><script><expr> <C-E> copilot#Accept("\<CR>")
-        let g:copilot_no_tab_map = v:true
-
-        ]])
-    end,
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        enabled = true,
+        keymap = {
+          accept = "<C-e>",
+          dismiss = "<C-x>",
+        },
+      },
+    },
   },
 }
